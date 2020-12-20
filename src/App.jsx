8a853d8ca1c5632e87e1b9fb34c09301f8f1418b
Zondazx <mzxgd16@gmail.com>
2020-12-19 20:12:06 -0400

    feat: avoid rate-limiting, refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import React, { useReducer, useEffect } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 import List from './components/List';
 import InputWithLabel from './components/InputWithLabel';
 import useLocalStorageState from './hooks/useLocalStorageState';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
-
-const getAsyncStories = () => (
-  new Promise(resolve => {
-    setTimeout(() => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  })
-);
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const storiesReducer = (state, action) => {
   switch(action.type) {
@@ -68,27 +43,35 @@ const App = () => {
     storiesReducer,
     { data: [], isLoading: false, isError: false },
   );
-  const [searchTerm, setSearchTerm] = useLocalStorageState('search', 'React'); 
+  const [searchTerm, setSearchTerm] = useLocalStorageState('search', ''); 
+  const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
 
-  useEffect(() => {
+  const handleFetchStories = useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
-      .then(result => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
-        });
-      })
+    fetch(url)
+      .then(response => response.json())
+        .then(result => {
+          dispatchStories({
+            type: 'STORIES_FETCH_SUCCESS',
+            payload: result.hits,
+          });
+        })
       .catch(() => {
-        dispatchStories({
-          type: 'STORIES_FETCH_FAILURE',
-        });
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
       });
-  }, []);
+  }, [url]);
+
+  useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
   const handleRemove = id => {
@@ -97,12 +80,6 @@ const App = () => {
       payload: id,
     });
   };
-
-  const searchedStories = stories.data.filter(story => 
-    story.title
-      .toLowerCase()
-      .includes(searchTerm.toLowerCase())
-  ); 
 
   return (
     <div>
@@ -118,6 +95,14 @@ const App = () => {
         Search: 
       </InputWithLabel>
 
+      <button
+        type='button'
+        disabled={!searchTerm}
+        onClick={handleSearchSubmit}
+      >
+        Submit
+      </button>
+
       <hr/>
       
       {stories.isError && <p>Something went wrong...</p>}
@@ -126,7 +111,7 @@ const App = () => {
         {stories.isLoading ? (
           <p>Loading...</p>
         ) : (
-          <List list={searchedStories} onRemove={handleRemove}/>
+          <List list={stories.data} onRemove={handleRemove}/>
         )}
       </ul>
 
